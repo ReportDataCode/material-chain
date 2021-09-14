3aa3c42a01aebb34a1f9c24f1ba8e32389b70434
Mark Jesper Pillerva <mark123jesper@yahoo.com>
2021-09-14 20:27:12 +0800

    Patched things up including nav responsiveness

---
+++ resources/js/components/Navigation.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
+const Navigation = () => {
+
+    const user = false;
+
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container fluid>
+                <Navbar.Brand as={Link} to='/'>React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to='/'>
+                            <i className="fas fa-home"></i> Home
+                        </Nav.Link>
+                        <Nav.Link as={Link} to='/watch'>
+                            <i className="fas fa-play-circle"></i> Watch
+                        </Nav.Link>
+                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                            <NavDropdown.Item as={Link} to='/'>Action</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to='/'>Another action</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        {
+                            user
+                                ?
+                                <NavDropdown title={<><i className="fas fa-user-circle"></i> User</>} id="collasible-nav-dropdown" align="end">
+                                    <NavDropdown.Item as={Link} to='/profile'>Profile</NavDropdown.Item>
+                                    <NavDropdown.Item as={Link} to='/'>Logout</NavDropdown.Item>
+                                </NavDropdown>
+                                :
+                                <NavDropdown title="Account" id="collasible-nav-dropdown" align="end">
+                                    <NavDropdown.Item as={Link} to='/signin'>Sign in</NavDropdown.Item>
+                                    <NavDropdown.Item as={Link} to='/signup'>Sign up</NavDropdown.Item>
+                                </NavDropdown>
+                        }
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Navigation
